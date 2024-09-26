3a75a12bcef72f996b7a45c80e80e4ab6857c218
jyroy <jyroooy@163.com>
2024-09-26 08:51:47 +0800

    support veterize rms norm

--- llminfer/source/op/kernels/cuda/rmsnorm_kernel.cu
+++ llminfer/source/op/kernels/cuda/rmsnorm_kernel.cu
@@ -62,6 +62,60 @@ static __global__ void row_rmsnorm_f32_block_reduce(
   }
 }
 
+static __global__ void row_rmsnorm_f32_veterize(
+    float* in,
+    float* wei,
+    float* out,
+    int size,
+    float eps) {
+  const int tid = threadIdx.x;
+  constexpr int pack_size = 4;
+  const int pack_num = size / pack_size;
+  const int pack_off = pack_size * pack_num;
+
+  float sum = 0.0f;
+  float4* in_pack = reinterpret_cast<float4*>(in);
+
+  for (int i = tid; i < pack_num; i += blockDim.x) {
+    float4 in_float4 = *(in_pack + i);
+    sum += in_float4.x * in_float4.x;
+    sum += in_float4.y * in_float4.y;
+    sum += in_float4.z * in_float4.z;
+    sum += in_float4.w * in_float4.w;
+  }
+
+  for (int i = pack_off + tid; i < size; i += blockDim.x) {
+    sum += in[i] * in[i];
+  }
+
+  using BlockReduce = cub::BlockReduce<float, 128>;
+  __shared__ typename BlockReduce::TempStorage temp;
+  __shared__ float shared_val;
+  sum = BlockReduce(temp).Sum(sum);
+  if (threadIdx.x == 0) {
+    shared_val = sum;
+  }
+
+  __syncthreads();
+  sum = shared_val;
+  const float scale = rsqrtf(sum / static_cast<float>(size) + eps);
+  float4* wei_pack = reinterpret_cast<float4*>(wei);
+  float4* out_pack = reinterpret_cast<float4*>(out);
+  for (int i = tid; i < pack_num; i += blockDim.x) {
+    float4 in_float4 = *(in_pack + i);
+    float4 wei_float4 = *(wei_pack + i);
+    *(out_pack + i) = make_float4(
+        scale * in_float4.x * wei_float4.x,
+        scale * in_float4.y * wei_float4.y,
+        scale * in_float4.z * wei_float4.z,
+        scale * in_float4.w * wei_float4.w);
+  }
+
+  for (int i = pack_off + tid; i < size; i += blockDim.x) {
+    out[i] = wei[i] * in[i] * scale;
+  }
+}
+
 void rmsnorm_kernel_cuda(
     const tensor::Tensor& input,
     const tensor::Tensor& weight,
@@ -84,10 +138,10 @@ void rmsnorm_kernel_cuda(
   constexpr int threads_num = 128;
   if (stream) {
     cudaStream_t stream_ = static_cast<cudaStream_t>(stream);
-    row_rmsnorm_f32_block_reduce<<<1, threads_num, 0, stream_>>>(
+    row_rmsnorm_f32_veterize<<<1, threads_num, 0, stream_>>>(
         input_ptr, weight_ptr, output_ptr, size, eps);
   } else {
-    row_rmsnorm_f32_block_reduce<<<1, threads_num>>>(
+    row_rmsnorm_f32_veterize<<<1, threads_num>>>(
         input_ptr, weight_ptr, output_ptr, size, eps);
   }
 }
