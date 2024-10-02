73e7333f471c9e08cf0027b5eeefbf7e46f669e1
jyroy <jyroooy@163.com>
2024-10-03 00:21:01 +0800

    support cuda memory buffer

--- llminfer/source/base/alloc_cuda.cpp
+++ llminfer/source/base/alloc_cuda.cpp
@@ -1,4 +1,5 @@
 #include <cuda_runtime_api.h>
+#include <glog/logging.h>
 #include "base/alloc.h"
 
 namespace base {
@@ -7,12 +8,78 @@ CUDADeviceAllocator::CUDADeviceAllocator()
     : DeviceAllocator(DeviceType::kDeviceCUDA) {}
 
 void* CUDADeviceAllocator::allocate(size_t byte_size) const {
-  if (!byte_size) {
-    return nullptr;
+  /*old version
+    always use cudaMalloc to malloc cuda memory
+    for trivial memory, calling cudaMalloc api all the time is inefficient
+  */
+  // if (!byte_size) {
+  //   return nullptr;
+  // }
+
+  // void* ptr = nullptr;
+  // cudaError_t err = cudaMalloc(&ptr, byte_size);
+  // CHECK_EQ(err, cudaSuccess);
+  // return ptr;
+
+  /*new version
+  use memory buffer to manage
+  */
+  int device_id = -1;
+  cudaError state = cudaGetDevice(&device_id);
+  if (byte_size > 1024 * 1024) { // > 1mb
+    auto& big_buffers = big_buffers_map_[device_id];
+    int sel_id = -1;
+    for (int i = 0; i < big_buffers.size(); i++) {
+      if (big_buffers[i].byte_size >= byte_size && !big_buffers[i].busy &&
+          big_buffers[i].byte_size < 1024 * 1024) {
+        sel_id = i;
+      }
+    }
+
+    if (sel_id != -1) {
+      big_buffers[sel_id].busy = true;
+      return big_buffers[sel_id].data;
+    }
+
+    void* ptr = nullptr;
+    state = cudaMalloc(&ptr, byte_size);
+    if (cudaSuccess != state) {
+      char buf[256];
+      snprintf(
+          buf,
+          256,
+          "Error: CUDA error when allocating %lu MB memory! maybe there's no enough memory "
+          "left on  device.",
+          byte_size >> 20);
+      LOG(ERROR) << buf;
+      return nullptr;
+    }
+    big_buffers.emplace_back(ptr, byte_size, true);
+    return ptr;
   }
 
+  auto& cuda_buffers = cuda_buffers_map_[device_id];
+  for (int i = 0; i < cuda_buffers.size(); i++) {
+    if (cuda_buffers[i].byte_size >= byte_size && !cuda_buffers[i].busy) {
+      cuda_buffers[i].busy = true;
+      no_busy_cnt_[device_id] -= cuda_buffers[i].byte_size;
+      return cuda_buffers[i].data;
+    }
+  }
   void* ptr = nullptr;
-  cudaError_t err = cudaMalloc(&ptr, byte_size);
+  state = cudaMalloc(&ptr, byte_size);
+  if (cudaSuccess != state) {
+    char buf[256];
+    snprintf(
+        buf,
+        256,
+        "Error: CUDA error when allocating %lu MB memory! maybe there's no enough memory "
+        "left on  device.",
+        byte_size >> 20);
+    LOG(ERROR) << buf;
+    return nullptr;
+  }
+  cuda_buffers.emplace_back(ptr, byte_size, true);
   return ptr;
 }
 
